d1779ecac422ddb75bf06c1bb359b53b68fad2ff
Marco Brandizi <marco.brandizi@gmail.com>
2019-03-05 19:37:52 +0000

    Fixing Travis file.

--- build-all.sh
+++ build-all.sh
@@ -1,5 +1,12 @@
 cd "$(dirname $0)"
-goal="${1-verify}" 
+goal="${1-verify}"
+shift 
+
+echo -e "\n\n\tInitialising the data server\n"
 ./resources-init.sh
-mvn $goal
+
+echo -e "\n\n\tBuilding\n"
+mvn $goal $*
+
+echo -e "\n\n\tTerminating the data server\n"
 ./resources-shutdown.sh
