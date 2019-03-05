74bd96238fd90197a8ab3ac1b5c76da3b446ef62
Marco Brandizi <marco.brandizi@gmail.com>
2019-03-05 21:53:39 +0000

    Minimal changes of build messages.

--- build-all.sh
+++ build-all.sh
@@ -2,11 +2,11 @@ cd "$(dirname $0)"
 goal="${1-verify}"
 shift 
 
-echo -e "\n\n\tInitialising the data server\n"
+echo -e "\n\n\tInitialising resources (data server etc)\n"
 ./resources-init.sh
 
 echo -e "\n\n\tBuilding\n"
 mvn $goal $*
 
-echo -e "\n\n\tTerminating the data server\n"
+echo -e "\n\n\tTerminating resources\n"
 ./resources-shutdown.sh
