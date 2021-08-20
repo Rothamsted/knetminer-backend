519a592d57114816181d54865df4fbbd7177769f
marco-brandizi <marco.brandizi@gmail.com>
2021-08-20 20:24:06 +0100

    RRes pipeline, Neo4j server update script.

--- rres-endpoints/servers-sync.sh
+++ rres-endpoints/servers-sync.sh
@@ -9,5 +9,11 @@ secret=`cat "$KNET_WEB_SECRETS_DIR/$KNET_DATASET_ID-$KNET_DATASET_VERSION.key"`
 web_target="$KNET_DOWNLOAD_SSH:$KNET_DOWNLOAD_DIR/$KNET_DATASET_ID/$KNET_DATASET_VERSION/$secret"
 rsync --exclude=tmp $RSYNC_DFLT_OPTS $RSYNC_MIRROR_OPTS "$KNET_DATASET_TARGET/" "$web_target"
 
+# TODO: babvs72/73
+
 echo -e "\n\n\tSynchronising file dump with Neo4j server\n"
 rsync $RSYNC_DFLT_OPTS $RSYNC_BKP_OPTS "$KNET_DATASET_TARGET/neo4j.dump" "$KNET_NEO_SERVER_SSH:$KNET_NEO_SERVER_DATA_DIR/$KNET_DATASET_ID-$KNET_DATASET_VERSION-neo4j.dump" 
+
+echo -e "\n\n\tRe-populating the Neo4j Server\n"
+./config/neo4j/neo-server-update-$KNET_DATASET_ID.sh
+
