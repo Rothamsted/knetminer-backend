326243eea16b3986674131c5241eb13be46c4604
Marco Brandizi <marco.brandizi@gmail.com>
2021-09-20 14:34:58 +0100

    Developing Cyverse scripts.

--- rres-endpoints/cyverse/load/generic-load.sh
+++ rres-endpoints/cyverse/load/generic-load.sh
@@ -5,7 +5,7 @@ echo -e "\n\n\tDownloading Ontologies\n"
 onto_dir="$CY_DATA_DIR/ontologies"
 mkdir -p "$onto_dir"
 
-"$CY_SCRIPTS_HOME/utils/knet-download.sh" "$data_url/rdf/ontologies/" "$onto_dir"
+"$CY_SCRIPTS_HOME/utils/knet-download.sh" "rdf/ontologies/" "$onto_dir"
 
 echo -e "\n\n\tReloading Ontologies\n"
 "$VIRTUOSO_UTILS_HOME/virt_load.sh" -r "$onto_dir" "${CY_DATASET_GRAPH_PREFIX}ontologies"
@@ -16,7 +16,7 @@ echo -e "\n\n\tDownloading Dataset\n"
 data_dir="$CY_DATA_DIR/$CY_DATASET_ID"
 mkdir -p "$data_dir"
 
-"$CY_SCRIPTS_HOME/utils/knet-download.sh" "$data_url/rdf/knowledge-graph.ttl.bz2" "$data_dir/${CY_DATASET_ID}.ttl.bz2"
+"$CY_SCRIPTS_HOME/utils/knet-download.sh" "rdf/knowledge-graph.ttl.bz2" "$data_dir/${CY_DATASET_ID}.ttl.bz2"
 
 echo -e "\n\n\tReloading the Dataset into Virtuoso\n"
 "$VIRTUOSO_UTILS_HOME/virt_load.sh" "$data_dir" "${CY_DATASET_GRAPH_PREFIX}${CY_DATASET_ID}"
