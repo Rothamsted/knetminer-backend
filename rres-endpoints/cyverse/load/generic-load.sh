0de57642050805d396b12fa67f71a70421b82c8c
Marco Brandizi <marco.brandizi@gmail.com>
2021-09-20 14:29:51 +0100

    Developing Cyverse scripts.

--- rres-endpoints/cyverse/load/generic-load.sh
+++ rres-endpoints/cyverse/load/generic-load.sh
@@ -1,24 +1,22 @@
 set -e
-wkey_file="$CY_WEB_SECRETS_DIR/$CY_DATASET_ID-$CY_DATASET_VERSION.key"
-[[ -e "$wkey_file" ]] && secret=`cat "$wkey_file"`
-
-[[ -z "$secret" ]] && data_url="$CY_DATA_URL/$CY_DATASET_ID/$CY_DATASET_VERSION" \
-|| data_url="$CY_PRIVATE_DATA_URL/$CY_DATASET_ID/$CY_DATASET_VERSION/$secret"
-
-
-data_url="$data_url/rdf"
-
 
 echo -e "\n\n\tDownloading Ontologies\n"
 
 onto_dir="$CY_DATA_DIR/ontologies"
 mkdir -p "$onto_dir"
 
-lftp="set ssl:verify-certificate no;"
-lftp="$lftp mirror -v --continue $data_url/ontologies/ '$onto_dir';"
-lftp="$lftp exit"
-
-lftp -e "$lftp"
+"$CY_SCRIPTS_HOME/utils/knet-download.sh" "$data_url/rdf/ontologies/" "$onto_dir"
 
 echo -e "\n\n\tReloading Ontologies\n"
 "$VIRTUOSO_UTILS_HOME/virt_load.sh" -r "$onto_dir" "${CY_DATASET_GRAPH_PREFIX}ontologies"
+
+
+echo -e "\n\n\tDownloading Dataset\n"
+
+data_dir="$CY_DATA_DIR/$CY_DATASET_ID"
+mkdir -p "$data_dir"
+
+"$CY_SCRIPTS_HOME/utils/knet-download.sh" "$data_url/rdf/knowledge-graph.ttl.bz2" "$data_dir/${CY_DATASET_ID}.ttl.bz2"
+
+echo -e "\n\n\tReloading the Dataset into Virtuoso\n"
+"$VIRTUOSO_UTILS_HOME/virt_load.sh" "$data_dir" "${CY_DATASET_GRAPH_PREFIX}${CY_DATASET_ID}"
---
+++ rres-endpoints/cyverse/utils/knet-download.sh
@@ -0,0 +1,29 @@
+set -e
+
+[[ "$#" != 2 ]] && {
+	echo -e "\n\nDownloads from The Knetminer data download URL\n"
+	echo -e "\t$0 <url path within the dataset dir> <local path>\n"
+
+	echo -e "\nFor URL directories, use a final '/',\n"
+	echo -e "for repeating their basename, use '/' for the local path\n\n"
+
+	exit 1 
+}
+
+url_path="$1"
+out_path="$2"
+
+
+wkey_file="$CY_WEB_SECRETS_DIR/$CY_DATASET_ID-$CY_DATASET_VERSION.key"
+[[ -e "$wkey_file" ]] && secret=`cat "$wkey_file"`
+
+[[ -z "$secret" ]] && data_url="$CY_DATA_URL/$CY_DATASET_ID/$CY_DATASET_VERSION" \
+|| data_url="$CY_PRIVATE_DATA_URL/$CY_DATASET_ID/$CY_DATASET_VERSION/$secret"
+
+data_url="$data_url"
+
+lftp="set ssl:verify-certificate no;"
+lftp="$lftp mirror -v --continue '$data_url/$url_path' '$out_path';"
+lftp="$lftp exit"
+lftp -e "$lftp"
+
