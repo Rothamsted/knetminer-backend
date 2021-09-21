f5aa7287903c5cc8d0651bb35d21f9d2980dfb22
Marco Brandizi <marco.brandizi@gmail.com>
2021-09-21 15:49:13 +0100

    Cyverse scripts, neo4j.

--- rres-endpoints/cyverse/load/neo-load.sh
+++ rres-endpoints/cyverse/load/neo-load.sh
@@ -21,12 +21,12 @@ echo -e "\n\n\tUpgrading Neo4j instance\n"
 
 cd "$NEO4J_HOME"
 
-echo -e "Stopping Neo4j\n"
-./bin/neo4j stop
-
 echo -e "Downloading the dump\n"
 dump_path=/tmp/neo4j.dump
 "$CY_SCRIPTS_HOME/utils/knet-download.sh" "neo4j.dump" "$dump_path"
+
+echo -e "Stopping Neo4j\n"
+./bin/neo4j stop
 
 echo -e "Uploading Neo4j\n"
 ./bin/neo4j-admin load --force --from="$dump_path"
