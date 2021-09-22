63f7b5750f77049b0e1d99db4d3f1bcf171e0269
Marco Brandizi <marco.brandizi@gmail.com>
2021-09-22 18:33:53 +0000

    Updating cyverse sys-bkp script.

--- rres-endpoints/cyverse/utils/sys-bkp.sh
+++ rres-endpoints/cyverse/utils/sys-bkp.sh
@@ -10,12 +10,14 @@ tar cv --bzip2 -f etc.tar.bz2 /etc
 tar cv --bzip2 -f software.tar.bz2 \
   --exclude '/opt/software/neo4j-*/data' \
   /opt/software
-tar cv --bzip2 -f root.tar.bz2 --exclude /root/tmp /root
+tar cv --bzip2 -f root.tar.bz2 --exclude /root/tmp --exclude /root/.cache /root
+
+if [[ "$bkp_name" == '3store' ]]; then
+  tar cv --bzip2 -f data.tar.bz2 /data/virtuoso/db/virtuoso.ini
+fi
 
 dpkg --get-selections >packages.lst
 
 cd ..
 rclone copy --progress "$bkp_dir" "rres_onedrive:bkp/cyverse-$bkp_name"
 rm -Rf "$bkp_dir"
-
- 
