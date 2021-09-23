1a81b66f2954eb50d1716af3bc202c6d670335aa
Marco Brandizi <marco.brandizi@gmail.com>
2021-09-23 13:59:37 +0100

    Cyverse scripts, neo4j.

--- rres-endpoints/cyverse/config/init-dataset-cfg.sh
+++ rres-endpoints/cyverse/config/init-dataset-cfg.sh
@@ -6,5 +6,9 @@ fi
 export CY_DATASET_ID="$1"
 export CY_DATASET_VERSION="$2"
 
-. "config/datasets/${CY_DATASET_ID}-${CY_DATASET_VERSION}-cfg.sh"
+for cfg_prefix in "${CY_DATASET_ID}-${CY_DATASET_VERSION}" "${CY_DATASET_ID}-common"
+do 
+  cfg_script="config/datasets/${cfg_prefix}-cfg.sh"
+	[[ -e "$cfg_script" ]] && . "$cfg_script"
+done
 . "config/dataset-cfg.sh"
--- rres-endpoints/cyverse/load/neo-load.sh
+++ rres-endpoints/cyverse/load/neo-load.sh
@@ -1,39 +1,20 @@
-#
-# We use this to populate the Neo4j instances on our Cyverse servers, starting from dump files uploaded on line
-# (currently, on OneDrive).
-#
-# TODO: comment the code
-#
 set -e
-TRACKER=/tmp/neo-load.running
 
-if [[ -e "$TRACKER" ]]; then
-  echo -e "\n  Script is already running, as per '$TRACKER', quitting\n"
-  exit
-fi
+echo -e "\n\n\tUpgrading Neo4j instance at '$CY_DATASET_NEO_HOME'\n"
 
-echo 1 >"$TRACKER"
-
-export NEO4J_HOME=${NEO4J_HOME:=/opt/software/neo4j}
-# You can also use NEO_TAR_OPTS
-
-echo -e "\n\n\tUpgrading Neo4j instance\n"
-
-cd "$NEO4J_HOME"
+cd "$CY_DATASET_NEO_HOME"
 
 echo -e "Downloading the dump\n"
 dump_path="$CY_TMP/neo4j.dump"
 "$CY_SCRIPTS_HOME/utils/knet-download.sh" "neo4j.dump" "$dump_path"
 
-echo -e "Stopping Neo4j\n"
-./bin/neo4j stop
+# Stop it
+"$CY_SCRIPTS_HOME/utils/neo-servers-service.sh stop "$CY_DATASET_NEO_HOME"
 
 echo -e "Uploading Neo4j\n"
 ./bin/neo4j-admin load --force --from="$dump_path"
 
-echo -e "Restarting Neo4j\n"
-ulimit -n 40000
-./bin/neo4j start
+# Restart
+"$CY_SCRIPTS_HOME/utils/neo-servers-service.sh start "$CY_DATASET_NEO_HOME"
 
-rm -f "$TRACKER"
 echo -e "\n\n\tNeo4j Updated\n"
