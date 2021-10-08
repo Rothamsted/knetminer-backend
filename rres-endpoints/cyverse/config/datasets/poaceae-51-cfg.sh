ed5f763b52ed5760642470258005e79344359aae
Marco Brandizi <marco.brandizi@gmail.com>
2021-10-08 18:00:17 +0000

    Cyverse, debugging virtuoso scripts.

--- rres-endpoints/cyverse/config/datasets/poaceae-51-cfg.sh
+++ rres-endpoints/cyverse/config/datasets/poaceae-51-cfg.sh
@@ -1,3 +1,4 @@
-# Nothing specific to do, putting the file here just to mark this dataset and version
-# exist.
-#
+# Define it when your dataset has an agrischema mapping file. 
+# this is used as tail for the named graph URI
+#  
+export CY_DATASET_AGSCHEMA_ID="knetAgrischemas"
--- rres-endpoints/cyverse/load/3sload.sh
+++ rres-endpoints/cyverse/load/3sload.sh
@@ -15,13 +15,30 @@ mkdir -p "$onto_dir"
 echo -e "\n\n\tReloading Ontologies\n"
 "$VIRTUOSO_UTILS_HOME/virt_load.sh" -r "$onto_dir" "${CY_DATASET_GRAPH_PREFIX}ontologies"
 
+# Define this variable to skip the dataset and work on the agrischemas mappings only.
+# This is useful when you do the updates in two stages
+# 
+if [[ -z "$CY_DATASET_AGSCHEMA_ONLY" ]]; then
 
-echo -e "\n\n\tDownloading Dataset\n"
+	echo -e "\n\n\tDownloading Dataset\n"
 
-data_dir="$CY_DATA_DIR/rdf/$CY_DATASET_ID"
-mkdir -p "$data_dir"
+	data_dir="$CY_DATA_DIR/rdf/$CY_DATASET_ID"
+	mkdir -p "$data_dir"
 
-"$CY_SCRIPTS_HOME/utils/knet-download.sh" "rdf/knowledge-graph.ttl.bz2" "$data_dir/${CY_DATASET_ID}.ttl.bz2"
+	"$CY_SCRIPTS_HOME/utils/knet-download.sh" "rdf/knowledge-graph.ttl.bz2" "$data_dir/${CY_DATASET_ID}.ttl.bz2"
 
-echo -e "\n\n\tReloading the Dataset into Virtuoso\n"
-"$VIRTUOSO_UTILS_HOME/virt_load.sh" "$data_dir" "${CY_DATASET_GRAPH_PREFIX}${CY_DATASET_ID}"
+	echo -e "\n\n\tReloading the Dataset into Virtuoso\n"
+	"$VIRTUOSO_UTILS_HOME/virt_load.sh" "$data_dir" "${CY_DATASET_GRAPH_PREFIX}${CY_DATASET_ID}"
+fi
+
+# Agrischemas
+
+[[ -z "$CY_DATASET_AGSCHEMA_ID" ]] && exit 0
+
+echo -e "\n\n\tDownloading Agrischemas mappings\n"
+# we need to use its own dir, cause virt_load.sh uses ld_dir(), which only supports dirs
+mkdir -p "$data_dir/agrischemas"
+"$CY_SCRIPTS_HOME/utils/knet-download.sh" "rdf/agrischemas-map.nt.bz2" "$data_dir/agrischemas/${CY_DATASET_ID}-agrischemas.ttl.bz2"
+
+echo -e "\n\n\tReloading the Agrischemas mappings into Virtuoso\n"
+"$VIRTUOSO_UTILS_HOME/virt_load.sh" "$data_dir/agrischemas" "${CY_DATASET_GRAPH_PREFIX}${CY_DATASET_AGSCHEMA_ID}"
