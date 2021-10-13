c2829ef8891b14f4499e90a4a43588545af7c097
Marco Brandizi <marco.brandizi@gmail.com>
2021-10-13 19:53:12 +0100

    Working on gxa data. [ci skip]

--- rres-endpoints/cyverse/load/gxa-3sload.sh
+++ rres-endpoints/cyverse/load/gxa-3sload.sh
@@ -7,7 +7,7 @@ set -e
 data_dir="$CY_DATA_DIR/rdf/$CY_DATASET_ID"
 mkdir -p "$data_dir"
 
-"$CY_SCRIPTS_HOME/utils/knet-download.sh" --multi "rdf/" "$data_dir"
+"$CY_SCRIPTS_HOME/utils/knet-download.sh" --multi "/" "$data_dir"
 
 
 echo -e "\n\n\tReloading the Dataset into Virtuoso\n"
