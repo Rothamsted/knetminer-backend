f30d1f41880c95b408fc099418b0a7e271c61c9b
Marco Brandizi <marco.brandizi@gmail.com>
2021-10-13 19:25:21 +0100

    Working on gxa data publication. [ci skip]

--- rres-endpoints/config/datasets/gxa-v202110-cfg.sh
+++ rres-endpoints/config/datasets/gxa-v202110-cfg.sh
@@ -8,4 +8,5 @@
 # Eventually, it should point at
 export KNET_DATA_TARGET=/home/data/knetminer/pub/endpoints/agri-schemas
 export KNET_DOWNLOAD_DIR=/var/www/html/knetminer/downloads/agri-schemas
+export KNET_TEST_SERVERS=''
 export KNET_DATASET_HAS_NEO4J=false
--- rres-endpoints/servers-sync.sh
+++ rres-endpoints/servers-sync.sh
@@ -20,31 +20,32 @@ rsync --exclude=tmp --exclude='.*' $RSYNC_DFLT_OPTS $RSYNC_MIRROR_OPTS "$KNET_DA
 # The Neo4j stuff
 #
 
-[[ -z "$KNET_DATASET_HAS_NEO4J" ]] && exit
-
-
-echo -e "\n\n\tSynchronising file dump with Neo4j server\n"
-rsout=`rsync $RSYNC_DFLT_OPTS $RSYNC_BKP_OPTS "$KNET_DATASET_TARGET/neo4j.dump" \
-             "$KNET_NEO_SERVER_SSH:$KNET_NEO_SERVER_DATA_DIR/$KNET_DATASET_ID-$KNET_DATASET_VERSION-neo4j.dump" | tee /dev/tty`
-    
-# Synch Neo4j only if the dump was actually updated         
-if [[ ! "$rsout" =~ 'Number of regular files transferred: 0' ]]; then 
-
-  echo -e "\n\n\tRe-populating the Neo4j Server\n"
-  ./config/neo4j/neo-server-update-$KNET_DATASET_ID.sh
-
+if [[ "$KNET_DATASET_HAS_NEO4J" != 'false' ]]; then 
+	echo -e "\n\n\tSynchronising file dump with Neo4j server\n"
+	rsout=`rsync $RSYNC_DFLT_OPTS $RSYNC_BKP_OPTS "$KNET_DATASET_TARGET/neo4j.dump" \
+	             "$KNET_NEO_SERVER_SSH:$KNET_NEO_SERVER_DATA_DIR/$KNET_DATASET_ID-$KNET_DATASET_VERSION-neo4j.dump" | tee /dev/tty`
+	    
+	# Synch Neo4j only if the dump was actually updated         
+	if [[ ! "$rsout" =~ 'Number of regular files transferred: 0' ]]; then 
+	
+	  echo -e "\n\n\tRe-populating the Neo4j Server\n"
+	  ./config/neo4j/neo-server-update-$KNET_DATASET_ID.sh
+	
+	fi
 fi
 
-echo -e "\n\n\tSynchronising OXL data on KnetMiner app servers (requires manual reload)\n"
-for host in $KNET_TEST_SERVERS
-do
-  echo -e "\nSynchronising OXL dump with '$host' Knetminer server\n"
-  rsout=`rsync $RSYNC_DFLT_OPTS $RSYNC_BKP_OPTS \
-        "$KNET_DATASET_TARGET/knowledge-graph-uris.oxl" \
-        "$KNET_SSH_USER@$host:$KNET_TEST_DATA_DIR/knowledge-network.oxl" | tee /dev/tty`
-
-  [[ "$rsout" =~ 'Number of regular files transferred: 0' ]] && continue
-
-  ./utils/knet-server-restart.sh $host
-
-done
+if [[ ! -z "$KNET_TEST_SERVERS" ]]; then
+	echo -e "\n\n\tSynchronising OXL data on KnetMiner app servers (requires manual reload)\n"
+	for host in $KNET_TEST_SERVERS
+	do
+	  echo -e "\nSynchronising OXL dump with '$host' Knetminer server\n"
+	  rsout=`rsync $RSYNC_DFLT_OPTS $RSYNC_BKP_OPTS \
+	        "$KNET_DATASET_TARGET/knowledge-graph-uris.oxl" \
+	        "$KNET_SSH_USER@$host:$KNET_TEST_DATA_DIR/knowledge-network.oxl" | tee /dev/tty`
+	
+	  [[ "$rsout" =~ 'Number of regular files transferred: 0' ]] && continue
+	
+	  ./utils/knet-server-restart.sh $host
+	
+	done
+fi
