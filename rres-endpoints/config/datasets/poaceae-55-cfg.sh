03e7967e5a714e5ab7f4ef54069fcfb8bc6e3685
marco-brandizi <marco.brandizi@gmail.com>
2023-03-05 09:13:52 +0000

    Adding poaceae 55.

---
+++ rres-endpoints/config/datasets/poaceae-55-cfg.sh
@@ -0,0 +1,2 @@
+#export KNET_SRC_OXL="$KNET_DATA_HOME/etl-test/plant/$KNET_DATASET_VERSION/organisms/combo/$KNET_DATASET_ID/knowledge-network.oxl"
+export KNET_SRC_OXL="$KNET_DATA_HOME/pub/final-data/release-$KNET_DATASET_VERSION/$KNET_DATASET_ID/$KNET_DATASET_ID-public-kg.oxl"
