bc1bf221f66fcbdc8135caa136d6098c1e0bd08f
marco-brandizi <marco.brandizi@rothamsted.ac.uk>
2023-03-06 09:08:04 +0000

    RRes endpoint, fixing issues with SSH on 72/73.

--- rres-endpoints/utils/knet-server-restart.sh
+++ rres-endpoints/utils/knet-server-restart.sh
@@ -9,9 +9,16 @@ if [[ -z "$host" ]]; then
 fi
 
 echo -e "\nRestarting $host\n"
+# We need this convoluted thing, that SSH with a personal user, then does sudo as docker
+# 'docker' is the user controlling everything about the instance.
+# 
+# If you try 'ssh docker' only, you'll get a 'password expired' message. We might have IT people 
+# fixing it, but this is quicker and less dependant on them.
+#
+sudo_cmd="docker -u docker --set-home --login --non-interactive"
 ssh "$KNET_SSH_USER@$host" 'bash -il' << EOT
 
 set -e
-sudo --non-interactive /opt/software/knetminer-ci/docker/dataset-cleanup.sh '$KNET_TEST_DATASET_DIR'
-sudo --non-interactive /opt/software/knetminer-ci/rres/dockerhub-deploy.sh
+$sudo_cmd /opt/software/knetminer-ci/docker/dataset-cleanup.sh  '$KNET_TEST_DATASET_DIR'
+$sudo_cmd /opt/software/knetminer-ci/rres/dockerhub-deploy.sh
 EOT
