ffca7cd9c772736699ffbd156aa1af62c136b3b2
Marco Brandizi <marco.brandizi@gmail.com>
2024-03-13 12:57:16 +0000

    RRes ETL, updating Server update scripts. [ci skip]

--- rres-endpoints/utils/neo4j/neo-server-update-poaceae-free.sh
+++ rres-endpoints/utils/neo4j/neo-server-update-poaceae-free.sh
@@ -15,7 +15,8 @@ cd /opt/neo4j-cereals/bin
 # This has entries in /etc/sudoers.d/neo4j to make it working without password
 sudo systemctl stop neo4j-cereals.service
 
-  ./neo4j-admin load --force --from="$KNET_NEO_DATA/$KETL_DATASET_ID-$KETL_DATASET_VERSION-neo4j.dump"
+./neo4j-admin database load --from-stdin --overwrite-destination=true neo4j \
+  <"$KNET_NEO_DATA/$KETL_DATASET_ID-$KETL_DATASET_VERSION-neo4j.dump"
 
 sudo systemctl start neo4j-cereals.service
 
