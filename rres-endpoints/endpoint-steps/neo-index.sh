c04572906cc43226af714b52f549ae16adb3e3e4
Marco Brandizi <marco.brandizi@gmail.com>
2024-09-24 23:04:47 +0100

    rres-endpoints/neo-index.sh, comments and improvements.

--- rres-endpoints/endpoint-steps/neo-index.sh
+++ rres-endpoints/endpoint-steps/neo-index.sh
@@ -29,17 +29,43 @@ cp -R -v "$KETL_HOME/config/knet-init"/* "$knet_cfg/config"
 # --neo-motifs is provisional, we need it until we can replace it with the new traverser.
 #
 printf "\n\n  Creating Neo indexing (full-text and semantic motifs)\n\n"
+
+# Comment this to skip the traverser. BE CAREFUL
+# neo_motifs_flag='--neo-motifs'
 "$KNET_INITIALIZER_HOME/knet-init.sh" \
--c "$knet_cfg/config/config-etl.yml" --neo-index=config:// --neo-motifs --in "$oxl_src"
+-c "$knet_cfg/config/config-etl.yml" --neo-index=config:// $neo_motifs_flag --in "$oxl_src"
+
 
 # Sam 2024/09/13: Run the Cypher query to generate stats node in Neo4j
 printf "\nRunning Cypher query to generate stats node in Neo4j\n"
 
+# TODO: this is too much stuff to stay here, move it to its own script
+
+# TODO: this is wrong, you're hardwiring the Neo version and location
+# and that's useless anyway, since you already have $NEO4J_HOME.
+# 
+# Also, it's not worth to change PATH in a script, you can obtain the same
+# in a cleaner way via:
+#
+#   "$NEO4J_HOME/bin/cypher-shell"
+#
+# or, if you need it often:
+# 
+#   cycmd="$NEO4J_HOME/bin/cypher-shell"
+#   "$cycmd" -a "$running_host" ...
+#
 export PATH=$PATH:$KNET_SOFTWARE/neo4j-community-5.20.0-etl/bin
-running_host=bolt://`cat "$KETL_OUT/tmp/neo4j-slurm.host"`:7687
+
+# TODO: remove. This is alredy available in "$neo_url", which was already used
+# above.
+# running_host=bolt://`cat "$KETL_OUT/tmp/neo4j-slurm.host"`:7687
 
 current_date=$(date +%Y-%m-%d)
 
+# TODO: Probably you want to count (n:Concept) - [] -> (m:Concept), not all 
+# the graph
+# TODO: why toString()? Shouldn't they be numeric properties?
+# TODO: to be reviewed against https://schema.org/Dataset ?
 query="MATCH (n)-[r]-()
 WITH count(distinct n) AS nodeCount, count(distinct r) AS edgeCount
 CREATE (s:Metadata { 
@@ -50,7 +76,7 @@ CREATE (s:Metadata {
     date: \"${current_date}\"
 })"
 
-cypher-shell -a "$running_host" -u "$KETL_NEO_USR" -p "$KETL_NEO_PWD" --format plain "$query"
+cypher-shell -a "$neo_url" -u "$KETL_NEO_USR" -p "$KETL_NEO_PWD" --format plain "$query"
 
 echo -e "\nAll Neo4j indexing and stats generation done\n"
 echo `date` >"$out_flag"
