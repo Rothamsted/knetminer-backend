d63270d58fe75582e612fc2481cd964c9f7536e4
holegar <hesam.holegar@gmail.com>
2024-11-29 10:01:24 +0000

    Creating Fungi-lite. Updating Ascomycota 58RC3

--- rres-endpoints/config/datasets/fungi-lite-60-cfg.sh
+++ rres-endpoints/config/datasets/fungi-lite-60-cfg.sh
@@ -1,12 +1,12 @@
-# /home/data/knetminer/etl-test/brassica/58/generic/knowledge-network.oxl
+# /home/data/knetminer/etl-test/fungi-lite/60/generic/knowledge-network.oxl
 
 # Unfortunately, there isn't consistence, so we can use KETL_DATASET_ID here
-oxl_home="$KNET_HOME/etl-test/ascomycota-premium/$KETL_DATASET_VERSION"
+oxl_home="$KNET_HOME/etl-test/fungi-lite/$KETL_DATASET_VERSION"
 
 export KETL_SRC_OXL="$oxl_home/generic/knowledge-network.oxl"
 
 # Sam 20240909 - New versioning convention
-export KETL_OUT="$KETL_OUT_HOME/$KETL_DATASET_ID/v$KETL_DATASET_VERSION-RC3"
+export KETL_OUT="$KETL_OUT_HOME/$KETL_DATASET_ID/v$KETL_DATASET_VERSION-RC1"
 
 ## Neo 
 #
@@ -18,7 +18,7 @@ export NEO4J_HOME="$KNET_SOFTWARE/neo4j-community-$KETL_NEO_VERSION-etl"
 #
 # The name within the code base, which identifies the config dir to be
 # used for the KnetMiner initialiser
-export KNET_INIT_DATASET_ID="ascomycota-premium"
+export KNET_INIT_DATASET_ID="fungi-lite"
 
 
 ##### Values for server-sync.sh
@@ -33,7 +33,7 @@ export KNET_INIT_DATASET_ID="ascomycota-premium"
 #
 
 # Test servers like babvs73
-export KNET_TESTINST_DATA_PATH=/opt/data/knetminer-datasets/ascomycota-premium
+export KNET_TESTINST_DATA_PATH=/opt/data/knetminer-datasets/fungi-lite
 # babvs73: based on old Traverser, available at knetminer.com/ci-test
 # babvs72: based on Neo4j+OXL Traverser, available at knetminer.com/ci-test-cypher
-export KNET_TESTINST_SSH="brandizim@babvs73.rothamsted.ac.uk brandizim@babvs72.rothamsted.ac.uk"
+# export KNET_TESTINST_SSH="brandizim@babvs73.rothamsted.ac.uk brandizim@babvs72.rothamsted.ac.uk"
