317f62e75ee287f93bf3b4a4614323b5b1f7d539
Marco Brandizi <marco.brandizi@gmail.com>
2025-05-08 13:31:04 +0100

    Workaround for expired SSL certificate

---
+++ ci-build/build-before.sh
@@ -0,0 +1,6 @@
+# The SSL for https://knetminer.rothamsted.ac.uk has expired and this is the only way
+# to make Maven behave. TODO: remove after renewal
+#
+export MAVEN_BUILD_ARGS="$MAVEN_BUILD_ARGS -Djavax.net.ssl.trustStore=$ci-build/untrusted-certs.jks"
+# The JKS contains the Java defaults and the troubled certificate, so no actual secrets.
+export MAVEN_BUILD_ARGS="$MAVEN_BUILD_ARGS -Djavax.net.ssl.trustStorePassword=not-a-problem"
