2adfdff952e7ba45923c998a45ba951ba4efb165
holegar <hesam.holegar@gmail.com>
2025-08-04 13:35:41 +0100

    Minor formatting update

--- rres-endpoints/config/datasets/cereals-premium-v61-RC1-cfg.sh
+++ rres-endpoints/config/datasets/cereals-premium-v61-RC1-cfg.sh
@@ -1,4 +1,4 @@
-export KETL_REL_NOTES="\
+export KETL_REL_NOTES="\n\
 ---------------------------\n\
 Version v61-RC1 01-08-2025:\n\
 \n\
@@ -9,6 +9,6 @@ Version v61-RC1 01-08-2025:\n\
 - Fixed character encoding for non-English letters is publication titles/authors/etc.\n\
 - Replaced hash code identifiers in Phenotype nodes with short LLM-generated labels. Read more: https://github.com/KnetMiner/knetML/tree/main/Entity_Relation_extraction/bedrock_phenotype_title\n\
 - Added release notes to the graphs!\n\
----------------------------\
+---------------------------\n\
 "
 . "$KETL_HOME/config/datasets/${KETL_DATASET_ID}-${KETL_DATASET_VERSION_NUM}-cfg.sh"
--- rres-endpoints/config/datasets/vegetables-premium-v61-RC2-cfg.sh
+++ rres-endpoints/config/datasets/vegetables-premium-v61-RC2-cfg.sh
@@ -1,20 +1,20 @@
-export KETL_REL_NOTES="\
+export KETL_REL_NOTES="\n\
 ---------------------------\n\
 Version v61-RC2 01-08-2025:\n\
 \n\
 - New semantic motif categories. Read more: https://github.com/KnetMiner/knetminer-schemas/tree/main/semantic-motif-taxonomy/knet-motif-categories-doc\n\
 - Added release notes to the graphs!\n\
----------------------------\
+---------------------------\n\
 Version v61-RC1 24-07-2025:\n\
 \n\
 - Removed redundant alternative names for Arabidopsis Thaliana (Thale Cress).\n\
 - Fixed character encoding for non-English letters is publication titles/authors/etc.\n\
 - Replaced hash code identifiers in Phenotype nodes with short LLM-generated labels. Read more: https://github.com/KnetMiner/knetML/tree/main/Entity_Relation_extraction/bedrock_phenotype_title\n\
----------------------------\
+---------------------------\n\
 Version v60-RC4 05-06-2025:\n\
 \n\
-- Added semantic motif categories.
----------------------------\
+- Added semantic motif categories.\n\
+---------------------------\n\
 "
 . "$KETL_HOME/config/datasets/${KETL_DATASET_ID}-${KETL_DATASET_VERSION_NUM}-cfg.sh"
 
