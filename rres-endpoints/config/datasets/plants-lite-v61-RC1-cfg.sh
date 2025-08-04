0109b9ebaa0dee22f63c813d31580402a00915f0
holegar <hesam.holegar@gmail.com>
2025-08-04 14:13:21 +0100

    Building Plants-lite v61-RC1

---
+++ rres-endpoints/config/datasets/plants-lite-v61-RC1-cfg.sh
@@ -0,0 +1,12 @@
+export KETL_REL_NOTES="\n\
+---------------------------\n\
+Version v61-RC1 04-08-2025:\n\
+\n\
+- New semantic motif categories. Read more: https://github.com/KnetMiner/knetminer-schemas/tree/main/semantic-motif-taxonomy/knet-motif-categories-doc\n\
+- Removed redundant alternative names for Arabidopsis Thaliana (Thale Cress).\n\
+- Fixed character encoding for non-English letters is publication titles/authors/etc.\n\
+- Replaced hash code identifiers in Phenotype nodes with short LLM-generated labels. Read more: https://github.com/KnetMiner/knetML/tree/main/Entity_Relation_extraction/bedrock_phenotype_title\n\
+- Added release notes to the graphs!\n\
+---------------------------\n\
+"
+. "$KETL_HOME/config/datasets/${KETL_DATASET_ID}-${KETL_DATASET_VERSION_NUM}-cfg.sh"
