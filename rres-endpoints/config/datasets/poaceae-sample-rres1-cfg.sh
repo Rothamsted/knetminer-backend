c14a170650de0aa65bf135356714a2048795e19f
Marco Brandizi (RRes-Roth) <brandizim2@rothhpc4-login.cluster.local>
2026-04-28 00:15:25 +0100

    fix(rres endpoints): fix poaceae-sample-rres1 config

--- rres-endpoints/config/datasets/poaceae-sample-rres1-cfg.sh
+++ rres-endpoints/config/datasets/poaceae-sample-rres1-cfg.sh
@@ -2,9 +2,26 @@
 # As said elsewhere, this is done in dataset config files, to keep track of which Neo4j 
 # was used for each dataset
 
-version_no="$(echo "$KETL_DATASET_VERSION" | sed -E s/'^([0-9]+).*'/'\1'/)"
-. "$KETL_HOME/config/datasets/$KETL_DATASET_ID-$version_no-cfg.sh"
+version_no="$(echo "$KETL_DATASET_VERSION" | sed -E s/'^.*([0-9]+).*'/'\1'/)"
+. "$KETL_HOME/config/datasets/$KETL_DATASET_ID-${version_no}-cfg.sh"
 
 export KETL_HAS_NEO4J=true
-export KETL_NEO_VERSION='5.26.0'
-export NEO4J_HOME="$KNET_SOFTWARE/neo4j-community-$KETL_NEO_VERSION-etl"
+export KETL_NEO_VERSION='2026.04.0'
+export NEO4J_HOME="$KNET_SOFTWARE/neo4j-community-$KETL_NEO_VERSION"
+
+# TODO: remove, it's a temp hack to avoid a clash with another running pipeline
+# 
+function _ketl_get_neo_url ()
+{
+	host_file="$KETL_OUT/tmp/neo4j-slurm.host"
+	if [[ ! -f "$host_file" ]]; then
+		# it's probably down, we signal it this way
+		echo ''
+		return
+	fi
+  neo_host="$(cat "$host_file")"
+  echo "bolt://$neo_host:7688"
+}
+export -f _ketl_get_neo_url
+
+export KETL_NEO_IDX_PAUSE_TIME=1m
