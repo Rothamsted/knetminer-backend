2fe67ebad9edd9411bf959432d454f349c0bde35
holegar <hesam.holegar@gmail.com>
2026-06-30 14:18:10 +0100

    Updated to match new Neo4j version

--- rres-endpoints/config/datasets/vegetables-premium-common.sh
+++ rres-endpoints/config/datasets/vegetables-premium-common.sh
@@ -3,5 +3,5 @@ export KETL_SRC_OXL="$oxl_home/generic/knowledge-network.oxl"
 export KETL_OUT="$KETL_OUT_HOME/$KETL_DATASET_ID/$KETL_DATASET_VERSION"
 
 export KETL_HAS_NEO4J=true
-export KETL_NEO_VERSION='2026.04.0'
+export KETL_NEO_VERSION='2026.05.0'
 export NEO4J_HOME="$KNET_SOFTWARE/neo4j-community-$KETL_NEO_VERSION"
