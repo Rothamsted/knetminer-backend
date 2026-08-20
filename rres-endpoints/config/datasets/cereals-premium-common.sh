e8509139660eb4ded614849e9e1aa9327dae51f6
holegar <hesam.holegar@gmail.com>
2026-08-20 05:34:53 +0100

    chore: bump cereals/vegetables-premium to Neo4j 2026.05.0
    Also makes NEO4J_HOME resolve to a ROGER-relative path (with an
    ENDPOINT_ISOLATE override), matching the other datasets' common.sh.

--- rres-endpoints/config/datasets/cereals-premium-common.sh
+++ rres-endpoints/config/datasets/cereals-premium-common.sh
@@ -3,5 +3,5 @@ export KETL_SRC_OXL="$oxl_home/generic/knowledge-network.oxl"
 export KETL_OUT="$KETL_OUT_HOME/$KETL_DATASET_ID/$KETL_DATASET_VERSION"
 
 export KETL_HAS_NEO4J=true
-export KETL_NEO_VERSION='2026.04.0'
-export NEO4J_HOME="$KNET_SOFTWARE/neo4j-community-$KETL_NEO_VERSION"
+export KETL_NEO_VERSION='2026.05.0'
+export NEO4J_HOME="${KETL_NEO_HOME_OVERRIDE:-$KNET_HOME/etl-test/temp/neo4j-roger/neo4j-community-$KETL_NEO_VERSION}"
