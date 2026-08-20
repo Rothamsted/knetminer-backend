b810130b7bb37b3451b57bc32647c1885559f8ce
holegar <hesam.holegar@gmail.com>
2026-08-20 05:35:17 +0100

    feat: add fungi-premium-common.sh
    fungi-premium is a new KG (added in v63-RC1) and needs its own common.sh
    alongside the other datasets'.

---
+++ rres-endpoints/config/datasets/fungi-premium-common.sh
@@ -0,0 +1,7 @@
+oxl_home="$KNET_HOME/etl-test/$KETL_DATASET_ID/$KETL_DATASET_VERSION_NUM"
+export KETL_SRC_OXL="$oxl_home/generic/knowledge-network.oxl"
+export KETL_OUT="$KETL_OUT_HOME/$KETL_DATASET_ID/$KETL_DATASET_VERSION"
+
+export KETL_HAS_NEO4J=true
+export KETL_NEO_VERSION='2026.05.0'
+export NEO4J_HOME="${KETL_NEO_HOME_OVERRIDE:-$KNET_HOME/etl-test/temp/neo4j-roger/neo4j-community-$KETL_NEO_VERSION}"
